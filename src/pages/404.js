7bd41ca17721341060f3b6efd7b50df57d0190bb
oriverk <oriveryk@gmail.com>
2020-12-26 13:29:00 +0000

    create 404 not found paged

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,19 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import NOT_FOUND_IMG from '../../static/404.jpg'
+import '../styles/404.css'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404: Not found" description="ページが見つかりません。" />
+    <div className='not-found-message'>
+      <h1>404 Not Found</h1>
+      <Link to='/'>トップページ</Link>
+    </div>
+    <img src={NOT_FOUND_IMG} className='not-found-image' alt='not-found' />
   </Layout>
 )
 
