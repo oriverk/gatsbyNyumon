1298b789e7c02d3c4a7797e9e0252cfbf90ffff8
oriverk <oriveryk@gmail.com>
2020-12-26 09:10:40 +0000

    create posts index and these links component

---
+++ src/components/post-link.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "../styles/post-link.css"
+
+const RANDOM_IMG = "https://source.unsplash.com/collection/175083/640x360"
+
+export default function PostLink() {
+  return (
+    <div className="post-link">
+      <div>
+        <img
+          src={RANDOM_IMG}
+          className="post-link-image"
+          alt="post-cover"
+        ></img>
+      </div>
+      <div className="post-link-text">
+        <h2>I created a blog with Gatsby.</h2>
+        <p className="post-link-body">React製FWのGatsbyを学んでみました。</p>
+        <p className="post-link-date">2020年5月30日</p>
+      </div>
+    </div>
+  )
+}
