9ef99b6033f2fd336ef1f2a53c0096595a088687
oriverk <oriveryk@gmail.com>
2020-12-26 13:58:07 +0000

    yarn remove prop-types

--- src/components/seo.js
+++ src/components/seo.js
@@ -6,7 +6,6 @@
  */
 
 import React from "react"
-// import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
@@ -78,12 +77,5 @@ SEO.defaultProps = {
   meta: [],
   description: ``,
 }
-
-// SEO.propTypes = {
-//   description: PropTypes.string,
-//   lang: PropTypes.string,
-//   meta: PropTypes.arrayOf(PropTypes.object),
-//   title: PropTypes.string.isRequired,
-// }
 
 export default SEO
